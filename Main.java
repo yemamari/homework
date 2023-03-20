b7b8f8be691530d5da97ed9f20cf0be0a72b035c
yema.sewioh@gmail.com <yemaseiwoh@Yemas-MacBook-Air.local>
2023-03-20 18:23:11 -0400

    homework

---
+++ Main.java
@@ -0,0 +1,65 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String firstName ="Yema";
+         String lastName =" Seiwoh";
+         String full =firstName + lastName;
+        System.out.println(full);
+
+  int [] firstArray = {103,23,34,41,75};
+  int [] secondArray = {66,75,18,91,120};
+
+        System.out.println( firstArray[3]*secondArray[1]);
+firstArray[4]=69;
+ int answer = firstArray[4]/ secondArray[1];
+        System.out.println(answer);
+
+
+String[] music ={"sza","tems","omahlay"};
+int i =0;
+while (i<music.length){
+    System.out.println(music[i]);
+    i++;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
